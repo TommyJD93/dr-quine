67f4cd6b7385bb6e79c0a196ef271eccc1708a7b
Tommaso Terribili <tterribi@e3r9p1.42roma.it>
2024-12-12 20:02:21 +0100

    initial commit

---
+++ Colleen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// This is a comment
+
+char *str = "#inclue <stdio.h>%c "
+
+void print_source() {
+	printf();
+}
+
+int main() {
+	// This is a comment inside the main
+
+	print_program();
+}
